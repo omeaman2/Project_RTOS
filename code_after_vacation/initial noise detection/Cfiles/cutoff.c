2f7f67fd27a90ea893de735bb123c6bc5c9ec10b
maikel <maikel.witlox@quovide.com>
2019-11-15 14:20:07 +0100

    Added comments to cutoff.c

--- code_after_vacation/initial noise detection/Cfiles/cutoff.c
+++ code_after_vacation/initial noise detection/Cfiles/cutoff.c
@@ -13,24 +13,30 @@ int main() {
     unsigned long average = 0;
     unsigned long prevAverage = 0;
     unsigned int used = 0;
-    printf("arraySize: %d\n", arraySize);
+    
+    //Loop compolete array, safezone of 400 because next 400 elements are looped before check is reached
     for(int k = 0; k < (arraySize - 400); k+=400) {
         counter = 0;
         used = 0;
+	//Loop next 400 ellements calculate average
         for(int i = k; i < (k + 400); i++) {
             int data = abs(dataarray[i]);
-            if( data > 500 ) {
+            //If calue of data < 500 it can't be heard and is not usable
+	    if( data > 500 ) {
                 counter += data;
                 used++;
             }
         }
+	//Save avarage of last loop
         prevAverage = average;
+	//If used == 0 don't divide by 0, otherwise divide the counter by the amount of time the counter is edited.
         if(used != 0) {
             average = counter / used;
         } else {
             average = 0;
         }
         if(k != 0) {
+		//Check if an huge increase in volume occured.
             unsigned long safeZone = (prevAverage / 2);
             unsigned long tempAverage = prevAverage + safeZone;
 
@@ -44,27 +50,32 @@ int main() {
 int recognizeEnd(int start, unsigned long startMedium) {
     unsigned long average = 0;
     unsigned long counter = 0;
+    //max noise length is the start moment plus 0.5 second.
     unsigned long maxCount = start + 3000;
     unsigned long maxLoop = 0;
-    if(maxCount < arraySize) {	
+    //If the max noise length exeeds the arraysize, use arraysize - safezone as max noise length. Safezone is used because next 30 elements will be looped.
+    if(maxCount < arraySize) {
         maxLoop = maxCount;
     } else {
         maxLoop = arraySize - 30;
     }
     printf("start noise %d", start);
+    //Loop from start of noise to max 0.5 second further.
     for(int k = start; k < maxLoop; k++) {
         counter = 0;
+	//Check if noise drasticly decreases in next 0.05 seconds 
         for(int i = k; i < (k + 30); i++) {
             counter += abs(dataarray[i]);
         }
 
         average = counter / 30;
-
+	//If average is lower than the value at the start of the noise, noise ended so function is stopped.
         if(average <= startMedium) {
             printf(" end noise %d \n", k);
             return k;
         }
     }
+    //No end of noise is detected, detected noise get discarded.
     printf("end noise \n");
     return maxLoop;
 }
