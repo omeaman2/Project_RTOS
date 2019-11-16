a1a51f4c60b4a54c6761561619783099b992b258
Barend Mosch <moschbarend@gmail.com>
2019-11-16 15:03:15 +0100

    autocorrelation

--- code_after_vacation/initial noise detection/periodic/auto.c
+++ code_after_vacation/initial noise detection/periodic/auto.c
@@ -2,10 +2,10 @@
 #include<math.h>
 
 double* getAutoCorrelationOfSeries(double da[], int size);
-double GetCorrelaton(double x[], double y[]);
-double GetAverage(double data[]);
-double GetVariance(double data[]);
-double GetStdev(double data[]);
+double GetCorrelaton(double x[], double y[], int size_a, int size_b);
+double GetAverage(double data[], int size);
+double GetVariance(double data[], int size);
+double GetStdev(double data[], int size);
 
 int main(){
     double data_array[10] = {1, 4, 6, 3, 5, 6, 1, 5, 3, 3};
@@ -21,7 +21,7 @@ int main(){
     //     printf("%d - %f\n", i, *(data_array + i));
     // }
 
-    int size_result = sizeof(result) / sizeof(result[0]);
+    // int size_result = sizeof(result) / sizeof(result[0]);
     // printf("%d\n", size_result);
     // for (int i = 0; i < size_result; i++){
     //     printf("%d - %f\n", i, *(result + i));
@@ -35,34 +35,39 @@ int main(){
 double* getAutoCorrelationOfSeries(double da[], int size){
     int half = (int) (size / 2);
     int size_da = sizeof(da) / sizeof(da[0]);
-    printf("sizeof(da): %d -- size_da: %d\n", sizeof(da), size_da);
     double autoCorrelation[half];
-    double a[half];
-    double b[half];
+    double x[half];
+    double y[half];
+    int size_x, size_y;
 
     for (int i = 0; i < half; i++){
-        a[i] = da[i];
-        b[i] = da[i + i];
-        autoCorrelation[i] = GetCorrelaton(a, b);
+        x[i] = da[i];
+        y[i] = da[i + i];
+        size_x = sizeof(x) / sizeof(x[0]);
+        size_y = sizeof(y) / sizeof(y[0]);
+        autoCorrelation[i] = GetCorrelaton(x, y, size_x, size_y);
         //printf("%f - ", autoCorrelation[i]);
+    }
+
+    int size_result = sizeof(autoCorrelation) / sizeof(autoCorrelation[0]);
+    printf("%d\n", size_result);
+    for (int i = 0; i < size_result; i++){
+        printf("%d - %f\n", i, *(autoCorrelation + i));
     }
 
     return autoCorrelation;
 }
 
-double GetCorrelaton(double x[], double y[]) {
-    int size_x = sizeof(x) / sizeof(x[0]);
-    int size_y = sizeof(y) / sizeof(y[0]);
-    printf("%d\n%d\n", size_x, size_y);
+double GetCorrelaton(double x[], double y[], int size_x, int size_y) {
     if(size_x != size_y){
         printf("Length of sources is different");
         return 0;
     }
 
-    double avgX = GetAverage(x);
-    double stdevX = GetStdev(x);
-    double avgY = GetAverage(y);
-    double stdevY = GetStdev(y);
+    double avgX = GetAverage(x, size_x);
+    double stdevX = GetStdev(x, size_x);
+    double avgY = GetAverage(y, size_y);
+    double stdevY = GetStdev(y, size_y);
     double covXY = 0;
     double pearson = 0;
 
@@ -76,35 +81,31 @@ double GetCorrelaton(double x[], double y[]) {
     return pearson;
 }
 
-double GetAverage(double data[]){
-    size_t size_data = sizeof(data) / sizeof(data[0]);
-
-    if(size_data == 0){
+double GetAverage(double data[], int size){
+    if(size == 0){
         return 0;
     }
 
     double sum = 0;
 
-    for(int i = 0; i < size_data; i++){
+    for(int i = 0; i < size; i++){
         sum += data[i];
     }
 
-    return sum / size_data;
+    return sum / size;
 }
 
-double GetVariance(double data[]){
-    size_t size_data = sizeof(data) / sizeof(data[0]);
-
-    double avg = GetAverage(data);
+double GetVariance(double data[], int size){
+    double avg = GetAverage(data, size);
     double sum = 0;
 
-    for(int i = 0; i < size_data; i++){
+    for(int i = 0; i < size; i++){
         sum += pow((data[i] - avg), 2);
     }
 
-    return sum / size_data;
+    return sum / size;
 }
 
-double GetStdev(double data[]){
-    return sqrt(GetVariance(data));
+double GetStdev(double data[], int size){
+    return sqrt(GetVariance(data, size));
 }
