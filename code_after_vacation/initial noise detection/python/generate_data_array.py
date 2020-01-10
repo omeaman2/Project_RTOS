8cff388913340fcd1d53f9767df7e85a345fcf78
moschbarend <moschbarend@gmail.com>
2020-01-10 13:58:49 +0100

    copy write with scipy

--- code_after_vacation/initial noise detection/python/generate_data_array.py
+++ code_after_vacation/initial noise detection/python/generate_data_array.py
@@ -3,9 +3,17 @@ import numpy as np
 
 jump = 8
 
-a = wavfile.read('../../../resources/train_short.wav')
-data = np.array(a[1], dtype=float)
+samplerate, data = wavfile.read('../../../resources/train_short.wav')
+#data = np.array(a[1], dtype=float)
 print("data:\t\t", data)
+
+data_write = []
+for i in range(len(data)):
+    data_write.append(data[i])
+
+wavfile.write('../../../resources/new_data_array.wav', samplerate, data_write)
+
+'''
 f = open('../../../resources/data_array.txt', "w")
 for i in range(0, data.size, jump):
     # if (i == data.size - 1):
@@ -14,7 +22,7 @@ for i in range(0, data.size, jump):
     f.write("%d," % data[i])
 
 f.close()
-
+'''
 # samplerate = 44100 // jump
 # # samplerate = 44100
 
