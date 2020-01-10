b44fea7b347d647f57274ebc657eab10721093b3
yousousen <kaydo1@live.nl>
2020-01-10 15:32:58 +0100

    that's trivial

--- code_after_vacation/initial noise detection/python/generate_data_array_original.py
+++ code_after_vacation/initial noise detection/python/generate_data_array_original.py
@@ -1,20 +1,23 @@
-# trivial
 from scipy.io import wavfile
 import numpy as np
 import os
 
 jump = 1
 
-samplerate, data = wavfile.read('../../../resources/train_short.wav')
-#data = np.array(a[1], dtype=float)
+a = wavfile.read('../../../resources/train_short.wav')
+data = np.array(a[1], dtype=float)
 
+fdata_array = '../../../resources/data_array.txt'
+
+print("data:\t\t", data)
 f = open('../../../resources/data_array.txt', "w")
-
+f = open(fdata_array, "w")
 for i in range(0, data.size, jump):
     # if (i == data.size - 1):
         # f.write("%d" % data[i])
     # else:
     f.write("%d," % data[i])
+
 f.close()
 
 # Strip off last comma in data_array.txt.
