7f3c21a44f883c775c91d119a1a3062cf5833fb9
moschbarend <moschbarend@gmail.com>
2020-01-10 13:33:09 +0100

    added copy noise python file

---
+++ code_after_vacation/initial noise detection/python/read_copy_write_noise.py
@@ -0,0 +1,10 @@
+import sys, wave
+import soundfile as sf
+
+data, samplerate = sf.read('../../../resources/train_short.wav')
+
+data_write = []
+for i in range(len(data)):
+	data_write.append(data[i])
+
+sf.write('../../../resources/new_data_array.wav', data_write, samplerate)
