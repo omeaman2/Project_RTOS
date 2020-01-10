5e5ef674622a5d25623172998e3f4d961e04d558
moschbarend <moschbarend@gmail.com>
2020-01-10 15:25:27 +0100

    Updates original data_array

--- code_after_vacation/initial noise detection/python/generate_data_array_original.py
+++ code_after_vacation/initial noise detection/python/generate_data_array_original.py
@@ -3,10 +3,11 @@ import numpy as np
 
 jump = 1
 
-a = wavfile.read('../../../resources/train_short.wav')
-data = np.array(a[1], dtype=float)
-print("data:\t\t", data)
+samplerate, data = wavfile.read('../../../resources/train_short.wav')
+#data = np.array(a[1], dtype=float)
+
 f = open('../../../resources/data_array.txt', "w")
+
 for i in range(0, data.size, jump):
     # if (i == data.size - 1):
         # f.write("%d" % data[i])
