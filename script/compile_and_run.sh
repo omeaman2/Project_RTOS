4c38262cc387f44b59511cc1b0151e4f2ea8b758
yousousen <kaydo1@live.nl>
2020-03-11 14:59:27 +0100

    Stuff

--- script/compile_and_run.sh
+++ script/compile_and_run.sh
@@ -6,5 +6,4 @@ python 'code_after_vacation/initial noise detection/python/generate_data_arr.py'
 # Compiles and runs the c program. Creates to new_data_array.txt
 make all -C code_after_vacation/fft/using_the_kissfft_library/
 # Generates new_data_array.wav from new_data_array.txt
-python 'code_after_vacation/initial noise detection/python/after_cancellation.py'
-
+python 'code_after_vacation/output/after_cancellation.py'
